8c8c7d7b3d3b4b4133de966f66d09ba45f5a9791
Sirisha <chsiri100@gmail.com>
2026-04-27 19:06:00 -0400

    mcp server

--- src/mcpserver.py
+++ src/mcpserver.py
@@ -2,37 +2,14 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from jinja2 import Template
 import os
+from mcp.server.fastmcp import FastMCP
 
-app = FastAPI()
+server= FastMCP("prompt-engine")
 
-PROMPT_DIR = "prompts"
-
-
-# request body
-class PromptRequest(BaseModel):
-    name: str
-    code: str
-
-
-
-def load_template(name: str):
-    path = os.path.join(PROMPT_DIR, f"{name}.jinja")
-    print(path)
-    if not os.path.exists(path):
-        raise Exception(f"Prompt {name} not found")
-
-    with open(path, "r") as f:
-        return f.read()
-
-
-@app.post("/prompt")
-def render_prompt(req: PromptRequest):
-    template_str = load_template(req.name)
-    print(template_str)
-    template = Template(template_str)
-
-    rendered = template.render(code="abc")
-
-    return {
-        "prompt": rendered
-    }
+@server.prompt()
+def generate_architecture(text: str) -> str:
+    return f"""Please generate the architecture from the legacy code {code}, Thinking as "A".
+           Q: How to modernise legacy archtecture?
+           A: Please reference here : https://medium.com/cloud-journey-optimization/bridging-the-chasm-between-cloud-native-and-the-     	      mainframe-b87a2ed77742 """
+if __name__="__main__":
+   server.run()       
